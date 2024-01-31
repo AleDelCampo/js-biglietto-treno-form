8a0890031e49de10e00e9729e7a42fc6949b23ab
AleHashirama <alehashirama@gmail.com>
2024-01-31 11:30:58 +0100

    esercizio completato.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const button = document.querySelector("#create");
 
 const totalPrice = document.getElementById("total");
 
+const errorHappened = document.getElementById("error");
+
 let regularPrice;
 
 let finalPrice
@@ -20,20 +22,32 @@ create.addEventListener("click",
 
 create.addEventListener("click", 
     function () { 
-        regularPrice = km.value * 0.21;
 
-        finalPrice = regularPrice;
+        if ( ! (km.value < 1 )) {
 
-        if (age.value < 18) {
-            finalPrice = regularPrice - (regularPrice * 0.2);
-            totalPrice.innerHTML = "20% di sconto per i Minorenni!!"
-        } else if (age.value >= 65) {
-            finalPrice = regularPrice - (regularPrice * 0.4);
-            totalPrice.innerHTML = "40% di sconto per igli Anziani!!"
-        }   
+            regularPrice = km.value * 0.21;
 
-        finalPrice = finalPrice.toFixed(2)
+            finalPrice = regularPrice;
+            if (age.value < 18) {
+                finalPrice = regularPrice - (regularPrice * 0.2);
+                totalPrice.innerHTML = "20% di sconto per i Minorenni!!"
+            } else if (age.value >= 65) {
+                finalPrice = regularPrice - (regularPrice * 0.4);
+                totalPrice.innerHTML = "40% di sconto per igli Anziani!!"
+            }   
+    
+            finalPrice = finalPrice.toFixed(2)
+    
+            totalPrice.innerHTML += `Il totale del pagamento è di:€${finalPrice}`;
 
-        totalPrice.innerHTML += `Il totale del pagamento è di:€${finalPrice}`;
+            document.querySelector("#window").style.display = "block";
+
+            errorHappened.innerHTML = "";
+        } else {
+            errorHappened.innerHTML = "Ci stai prendendo in giro vero??";
+        }
+        
     }       
 )
+
+
